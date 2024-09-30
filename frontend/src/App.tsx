19efe6333f55db6a0a921ceb355b44f85cb5805d
Torleif Sandnes <tsa@itema.no>
2024-09-30 16:04:23 +0200

    Try to fix missing toast mobile

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -27,42 +27,44 @@ function App() {
   }, [notification, toast]);
 
   return (
-    <div className="bg-background text-foreground flex justify-center  items-center h-screen">
-      <section className="mx-auto flex flex-col items-center gap-2 py-8 md:py-12 md:pb-8 lg:py-24 lg:pb-20">
-        <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold">Passwordless login</h1>
-        <small className="text-sm text-gray-500">Authentication from the future</small>
-        <p className="text-sm md:text-base lg:text-lg text-center m-2 max-w-md ">
-          Register a user and log in with a single click. No passwords, I promise.
-        </p>
-        <div className="flex justify-around w-full p-2">
-          <Dialog open={isModalActive} onOpenChange={setIsModalActive}>
-            <DialogTrigger asChild>
-              <Button variant="secondary" onClick={() => setIsModalActive(true)}>
-                Register
-              </Button>
-            </DialogTrigger>
-            <RegistrationModalContent
-              setGlobalNotification={setNotification}
-              setIsModalActive={setIsModalActive}
-            />
-          </Dialog>
-          <Button
-            onClick={async () => {
-              const player = await assertPlayer();
-              if (player !== undefined) {
-                setNotification({
-                  heading: 'User was logged in',
-                  message: `${player.name} was logged in`,
-                });
-              }
-            }}
-          >
-            Log in
-          </Button>
-        </div>
-      </section>
+    <>
+      <div className="bg-background text-foreground flex justify-center  items-center h-screen">
+        <section className="mx-auto flex flex-col items-center gap-2 py-8 md:py-12 md:pb-8 lg:py-24 lg:pb-20">
+          <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold">Passwordless login</h1>
+          <small className="text-sm text-gray-500">Authentication from the future</small>
+          <p className="text-sm md:text-base lg:text-lg text-center m-2 max-w-md ">
+            Register a user and log in with a single click. No passwords, I promise.
+          </p>
+          <div className="flex justify-around w-full p-2">
+            <Dialog open={isModalActive} onOpenChange={setIsModalActive}>
+              <DialogTrigger asChild>
+                <Button variant="secondary" onClick={() => setIsModalActive(true)}>
+                  Register
+                </Button>
+              </DialogTrigger>
+              <RegistrationModalContent
+                setGlobalNotification={setNotification}
+                setIsModalActive={setIsModalActive}
+              />
+            </Dialog>
+            <Button
+              onClick={async () => {
+                const player = await assertPlayer();
+                if (player !== undefined) {
+                  setNotification({
+                    heading: 'User was logged in',
+                    message: `${player.name} was logged in`,
+                  });
+                }
+              }}
+            >
+              Log in
+            </Button>
+          </div>
+        </section>
+      </div>
       <Toaster />
-    </div>
+    </>
   );
 }
 
